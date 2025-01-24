fdda682ee03a6d980590adea7a2a87153073a784
Natalia Kurthan <kurthannatalia@gmail.com>
2025-01-24 14:13:51 +0300

    upd

--- src/components/fetchMovie/fetchMovie.js
+++ src/components/fetchMovie/fetchMovie.js
@@ -6,15 +6,35 @@ export const TMDB_API_KEY = import.meta.env.VITE_TMDB_API_KEY;
 axios.defaults.baseURL = "https://api.themoviedb.org";
 
 export async function fetchMovie(searchQuery, page) {
-  const data = await axios.get(`/3/search/movie`, {
-    params: {
+  try {
+    
+    console.log("TMDB_TOKEN:", TMDB_TOKEN);  
+    console.log("TMDB_API_KEY:", TMDB_API_KEY);  
+
+    
+    const requestParams = {
       query: searchQuery,
       page: page,
       api_key: TMDB_API_KEY,
-    },
-    headers: {
-      Authorization: `Bearer ${TMDB_TOKEN}`,
-    },
-  });
-  return data.data;
+    };
+    console.log("Request params:", requestParams);
+
+    
+    const data = await axios.get(`/3/search/movie`, {
+      params: requestParams,
+      headers: {
+        Authorization: `Bearer ${TMDB_TOKEN}`,
+      },
+    });
+
+  
+    console.log("Fetched movie data:", data);
+
+    return data.data;
+
+  } catch (error) {
+   
+    console.error("Error fetching movie data:", error.response || error);
+    throw error;
+  }
 }
