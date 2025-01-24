1b7974e217453cacc000d132b7662eb27c076a3c
Natalia Kurthan <kurthannatalia@gmail.com>
2025-01-24 12:50:49 +0300

    upd_api

--- src/api/api.js
+++ src/api/api.js
@@ -3,15 +3,25 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const TMDB_TOKEN = process.env.VITE_TMDB_TOKEN;
 const TMDB_API_KEY = process.env.VITE_TMDB_API_KEY;
 
 const api = axios.create({
-  baseURL: "https://api.themoviedb.org",
-  headers: {
-    Authorization: `Bearer ${TMDB_TOKEN}`,
-  },
+  baseURL: "https://api.themoviedb.org/3",
 });
 
+const getTrendingMovies = async () => {
+  try {
+    const response = await api.get(`/trending/movie/day`, {
+      params: {
+        language: "en-US",
+        api_key: TMDB_API_KEY,
+      },
+    });
+    console.log(response.data);
+  } catch (error) {
+    console.log("Error fetching data:", error);
+  }
+};
+
 export default api;
-export { TMDB_API_KEY };
+export { getTrendingMovies, TMDB_API_KEY };
